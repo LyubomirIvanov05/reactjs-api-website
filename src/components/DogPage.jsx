570c6276c2fd6fe7228febed5ab6fe9088a82fa6
LyubomirIvanov05 <livanov070805@gmail.com>
2024-11-29 12:41:07 +0000

    Fixed reload bug

--- src/components/DogPage.jsx
+++ src/components/DogPage.jsx
@@ -1,16 +1,38 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-const DogPage = ({ dogs }) => {
+const DogPage = () => {
+  const [dog, setDog] = useState([]);
   const { id } = useParams();
-  const dog = dogs.find((d) => d.id === id);
 
+
+console.log(id)
+
+  useEffect(() => {
+    async function fetchAPIData() {
+        const API_KEY = import.meta.env.VITE_API_KEY;
+        const url = `https://api.thedogapi.com/v1/images/${id}`;
+        const result = await fetch(url);
+        const breed = await result.json();
+
+        console.log(breed)
+        const dogObject = {
+          id: breed.id,
+          name: breed.breeds[0].name,
+          url: breed.url,
+        }
+        console.log(dogObject)
+        setDog(dogObject)
+    }
+
+    fetchAPIData();
+  }, []);
   if (!dog) {
     return <p>Dog not found!</p>;
   }
-
   return (
     <div>
-      <img src={dog.image} alt={dog.name} />
+      <img src={dog.url} alt={dog.name} />
       <h2>{dog.name}</h2>
     </div>
   );
